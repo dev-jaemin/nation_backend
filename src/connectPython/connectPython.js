ea5d493b40049fc1b44c5bb68b369f5527afa680
Taehoon Kim <twodf78@naver.com>
2022-07-18 14:09:33 +0000

    content

--- src/connectPython/connectPython.js
+++ src/connectPython/connectPython.js
@@ -8,29 +8,36 @@ import multer from "multer";
 const connectPython = {
     getContent: async (params) => {
         // params.gender == woman
-        const faceResult = child_process.spawnSync("python", [`./src/anAlyst/pickTheFace.py`, params.fileName, params.ext]);
+        // const faceResult = child_process.spawnSync("python", [`./src/anAlyst/pickTheFace.py`, params.fileName, params.ext]);
         console.log(params);
 
-        let faceFileName = faceResult.stdout.toString().replace("\n", "");
-        let res = fs.existsSync(faceFileName);
+        // let faceFileName = faceResult.stdout.toString().replace("\n", "");
+        // let res = fs.existsSync(faceFileName);
         let result = {
             gender: params.gender,
             class: params.testName,
             content: "No face",
+            img: "",
         };
-        if (!res) return result;
+        // if (!res) return result;
 
-        const pyResult = child_process.spawnSync("python", [`./src/anAlyst/kerasModel.py`, faceFileName, params.testName + "-" + params.gender]);
+        const pyResult = child_process.spawnSync("python", [
+            `./src/anAlyst/kerasModel.py`,
+            "./public/image/" + params.name,
+            params.testName + "-" + params.gender,
+        ]);
 
         let className = pyResult.stdout.toString().slice(147).slice(0, -2);
-
-        fs.unlink("./public/image/" + params.name, function () {});
-        fs.unlink(faceFileName, function () {});
+        console.log(className);
+        console.log(pyResult.stdout.toString());
+        // fs.unlink("./public/image/" + params.name, function () {});
+        // fs.unlink(faceFileName, function () {});
         let data = await pool.query(query.getContent, [className, params.gender]);
         console.log(data[0][0]);
 
         result.class = data[0][0].class;
         result.content = data[0][0][params.testName];
+        result.img = data[0][0].img;
         return result;
     },
 };
--- src/controller/imageController.js
+++ src/controller/imageController.js
@@ -6,12 +6,21 @@ import imageService from "../service/imageService.js";
 let name;
 let ext;
 // storage setting for file
+function checkFileName(str) {
+    var ext_temp = str.split(".").pop().toLowerCase();
+    console.log("ext_temp: " + ext_temp);
+}
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "public/image");
     },
     filename: (req, file, cb) => {
-        ext = path.extname(file.originalname);
+        // ext = path.extname(file.originalname);
+        checkFileName(file.originalname);
+        console.log(file.originalname);
+        ext = ".jpg";
+
+        console.log(ext);
         name = Date.now();
         cb(null, name + ext); // 파일 원본이름 저장
     },
@@ -29,8 +38,12 @@ const params = {
     ext: "",
 };
 router.post("/", upload.single("img"), async (req, res, next) => {
+    const header = {
+        "Access-Control-Allow-Origin": "",
+        "Access-Control-Allow-Credentials": true,
+    };
     const host = ["http://aitest.com:3000", "http://192.249.19.184:443", "http://domain.com:443"];
-    if (!host.includes(req.headers.origin)) return;
+    if (host.includes(req.headers.origin)) header["Access-Control-Allow-Origin"] = req.headers.origin;
 
     params.name = name + ext;
     params.gender = req.query.gender;
@@ -40,10 +53,6 @@ router.post("/", upload.single("img"), async (req, res, next) => {
 
     let rows = await imageService.uploadImage(params, res, next);
     console.log(req.headers.origin);
-    const header = {
-        "Access-Control-Allow-Origin": `${req.headers.origin}`,
-        "Access-Control-Allow-Credentials": true,
-    };
 
     return res.header(header).json(rows);
 });
